63802daed19e4f0e129557130e82df27a09550e7
thomasirmer <thomasirmer@gmail.com>
2014-07-08 10:40:25 +0200

    move ij.lib into project folder

--- src/CellAnnotationGUI.java
+++ src/CellAnnotationGUI.java
@@ -1,16 +1,13 @@
 import ij.IJ;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 import java.io.File;
-import java.io.FilenameFilter;
 
-import javax.swing.AbstractListModel;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -19,11 +16,7 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.ListCellRenderer;
-import javax.swing.ListModel;
 import javax.swing.ListSelectionModel;
-import javax.swing.event.ListDataListener;
-import javax.swing.filechooser.FileFilter;
 import javax.swing.border.EtchedBorder;
 
 @SuppressWarnings("serial")
